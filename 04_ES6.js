b5eeeecbd57818f3cc88b417b6b43fc6eb96247f
danmaril <dn.kr.ri.su@gmail.com>
2025-10-13 13:46:53 +0900

    10.13

--- 04_ES6.js
+++ 04_ES6.js
@@ -52,3 +52,64 @@ console.log("면적:", rect.area);
 
 rect.width = 15; 
 console.log("변경 후 면적:", rect.area);
+
+/* 3. Rest 파라미터와 Spread 문법 활용 */
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const mergeArrays = [ ...arr1, ...arr2];
+
+let sumAll = 0;
+for (const num of mergeArrays) {
+  sumAll += num;
+}
+console.log(`합계: ${sumAll} `);
+console.log('병합된 배열:', mergeArrays);
+
+/* 4. 구조분해 할당 활용 */
+const user = {
+  name: '홍길동',
+  age: 30,
+  locaton: '서울'
+}
+
+const [first, ...other] = [1, 2, 3, 4, 5];
+
+const {name, age} = user;
+
+
+console.log(`${name}은 ${age}살입니다.`)
+console.log(`첫 번째 요소: ${first}`);
+console.log(`나머지 요소: [${other}]`);
+
+/* 5. 클래스와 구조분해 할당을 활용한 학생 관리 시스템 */
+class Student {
+  constructor(name, age, score) {
+    this.name = name;
+    this.score = score;
+    this.age = age;
+  }
+}
+
+const studentA = new Student('유관순', 20, 90);
+const studentB = new Student('홍길동', 23, 80);
+const studentC = new Student('장보고', 25, 70);
+
+const students = [studentA, studentB, studentC];
+
+const names = students.map(({name}) => name);
+const scores = students.map(({score}) => score);
+
+console.log('학생 이름:', names.join(', '));
+console.log('학생 점수:', scores);
+
+/**
+ * 1. map 사용
+ * const names = students.map(({name}) => name);
+ * const scores = students.map(({score}) => score);
+ * 
+ * 2. for ...of 사용
+ * for (const{ name, score } of students) {
+ *    names.push(name);
+ *    scores.push(score);
+ * }
+ */
